5404d20f4f64acbb291aa0db5e5cb5bf7f3c2f90
KEERTHI RAJA <147988152+Srikeerthiraja@users.noreply.github.com>
2025-04-21 15:08:40 +0530

    Time: 0 ms (100%), Space: 46.6 MB (38.93%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,21 +1,35 @@
-// Approach 1: Precompute Left and Right Max Arrays (O(N) Time, O(N) Space)
 class Solution {
-    public int trap(int[] arr) {
-        int n = arr.length;
-        int[] lmax = new int[n];
-        lmax[0] = arr[0];
-        for (int i = 1; i < n; i++) 
-            lmax[i] = Math.max(lmax[i - 1], arr[i]);
+    public int trap(int[] height) {
+        int l=0;
+        int lm=0;
+        int rm=0;
+        int r=height.length-1;
+        int water=0;
+        while(l<r){
+            if(height[l]<height[r]){
+                if(height[l]>=lm){
+                    lm=height[l];
+                }
+                else{
+                    water+=lm-height[l];
+                }
+                l++;
+
+                
+
+            }
+            else{
+
+                if(height[r]>=rm){
+                    rm=height[r];
+                }
+                else{
+                    water+=rm-height[r];
+                }
+                r--;
+            }
+        }
+        return water;
         
-        int[] rmax = new int[n];
-        rmax[n - 1] = arr[n - 1];
-        for (int i = n - 2; i >= 0; i--) 
-            rmax[i] = Math.max(rmax[i + 1], arr[i]);
-
-        int ans = 0;
-        for (int i = 0; i < n; i++) 
-            ans += Math.min(lmax[i], rmax[i]) - arr[i];
-
-        return ans;
     }
 }
